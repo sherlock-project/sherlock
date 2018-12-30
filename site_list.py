4ecaad6289384fc4ee13a50c166de825958c8991
Christopher K. Hoadley <chris.hoadley@gmail.com>
2018-12-30 11:50:10 -0600

    Update script to use with-as structure for files.  Add number of supported sites to title. Add status message.

--- site_list.py
+++ site_list.py
@@ -1,15 +1,19 @@
+"""Sherlock: Supported Site Listing
+
+This module generates the listing of supported sites.
+"""
 import json
 
-raw = open("data.json", "r", encoding="utf-8")
-data = json.load(raw)
+with open("data.json", "r", encoding="utf-8") as data_file:
+    data = json.load(data_file)
 
-site_file = open('sites.md', 'w')
-site_file.write('## List of supported sites\n')
+with open("sites.md", "w") as site_file:
+    site_file.write(f'## List Of Supported Sites ({len(data)} Sites In Total!)\n')
 
-index = 1
-for social_network in data:
-    url_main = data.get(social_network).get("urlMain")
-    site_file.write(f'{index}. [{social_network}]({url_main})\n')
-    index = index + 1
+    index = 1
+    for social_network in data:
+        url_main = data.get(social_network).get("urlMain")
+        site_file.write(f'{index}. [{social_network}]({url_main})\n')
+        index = index + 1
 
-site_file.close()
+print("Finished updating supported site listing!")
