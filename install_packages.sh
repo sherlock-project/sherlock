d07df7b92c1ebf464428a8fce02499c3fcb21ed4
theodosisathanasakis <th.athanasakis@gmail.com>
2019-01-05 13:19:43 +0200

    Supports Debian/Ubuntu, Arch, Fedora,CentOS/RHEL and OpenSUSE based distos

--- install_packages.sh
+++ install_packages.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
-# install python3 if not exist
-sudo apt-get install python3
+# Determine which is the default package manager
+APT=$(which apt)
+PACMAN=$(which pacman)
+DNF=$(which dnf)
+YUM=$(which yum)
+ZYPPER=$(which zypper)
+
+# install python3 and pip3 if not exist
+if [ ${#APT} -gt 0 ]; then
+    sudo apt-get install python3
+    sudo apt-get install python3-pip
+elif [ ${#PACMAN} -gt 0 ]; then
+    echo bla
+    sudo pacman -S python3
+    sudo pacman -S python3-pip
+elif [ ${#DNF} -gt 0 ]; then
+    sudo dnf install python3
+    sudo dnf install python3-pip
+elif [ ${#YUM} -gt 0 ]; then
+    sudo yum install python3
+    sudo yum install python3-pip
+elif [ ${#ZYPPER} -gt 0 ]; then
+    sudo zypper install python3
+    sudo zypper install python3-pip
+else
+    echo "Unknown packge manager. Download one of the following:"
+    echo "  apt, pacman, dnf, yum or zypper"
+    echo ""
+    echo "or use README.md for instructions."
+    exit 1
+fi
+
 
 # install the all the necessery packages and requirements
-sudo apt-get install python3-pip
 sudo pip3 install --upgrade setuptools
 sudo pip3 install -r requirements.txt
-
 
