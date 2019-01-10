2247f556f2800267c9d41f66b2e7345ffbc2c39f
Yahya SayadArbabi <yahya.arbabi@gmail.com>
2019-01-10 17:17:17 +0330

    bump version

--- sherlock.py
+++ sherlock.py
@@ -23,7 +23,7 @@ from requests_futures.sessions import FuturesSession
 from torrequest import TorRequest
 
 module_name = "Sherlock: Find Usernames Across Social Networks"
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 amount=0
 
 # TODO: fix tumblr
