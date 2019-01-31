26ef2e1b9bc6b7162f3e6f196f68455585673393
Christopher K. Hoadley <chris.hoadley@gmail.com>
2019-01-30 18:10:06 -0600

    Convert Codementor to use the Status Code detection method. The site gives a clean 404 error.  Add to tests.

--- tests/all.py
+++ tests/all.py
@@ -160,7 +160,7 @@ class SherlockSiteCoverageTests(SherlockBaseTest):
         self.username_check(['noonewouldeverusethis7'],
                             ["Academia.edu", "9GAG", "About.me", "AngelList",
                              "BLIP.fm", "Bandcamp", "Behance", "BuzzFeed",
-                             "Codecademy"
+                             "Codecademy", "Codementor"
                             ],
                             exist_check=False
                            )
@@ -184,7 +184,7 @@ class SherlockSiteCoverageTests(SherlockBaseTest):
         self.username_check(['blue'],
                             ["Academia.edu", "9GAG", "About.me", "AngelList",
                              "BLIP.fm", "Bandcamp", "Behance", "BuzzFeed",
-                             "Codecademy"
+                             "Codecademy", "Codementor"
                             ],
                             exist_check=True
                            )
