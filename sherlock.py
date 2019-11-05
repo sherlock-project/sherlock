0cad205b902b46e62236d77f98e73fc75417fe0f
Christopher K. Hoadley <chris.hoadley@gmail.com>
2019-11-05 06:59:03 -0600

    Update version and site list.

--- sherlock.py
+++ sherlock.py
@@ -26,7 +26,7 @@ from torrequest import TorRequest
 from load_proxies import load_proxies_from_csv, check_proxy_list
 
 module_name = "Sherlock: Find Usernames Across Social Networks"
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 amount = 0
 
 
