e781ed5e260e1fdb58410a3103adb2b95ce547c8
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2020-03-14 13:58:47 +0100

    version bump 0.10.8 -- 0.10.9

--- sherlock.py
+++ sherlock.py
@@ -27,7 +27,7 @@ from torrequest import TorRequest
 from load_proxies import load_proxies_from_csv, check_proxy_list
 
 module_name = "Sherlock: Find Usernames Across Social Networks"
-__version__ = "0.10.8"
+__version__ = "0.10.9"
 
 
 global proxy_list
