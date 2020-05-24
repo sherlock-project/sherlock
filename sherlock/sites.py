cdfb97f6e674a5e619371bbddd1dbfdbb66a0346
탁동혁 <tdh8316@naver.com>
2020-05-24 12:35:04 +0900

    Use `is` operator when comparing singletons.

--- sherlock/sites.py
+++ sherlock/sites.py
@@ -208,7 +208,7 @@ class SitesInformation():
         List of strings containing names of sites.
         """
 
-        if popularity_rank == True:
+        if popularity_rank:
             #Sort in ascending popularity rank order.
             site_rank_name = \
                 sorted([(site.popularity_rank,site.name) for site in self],
