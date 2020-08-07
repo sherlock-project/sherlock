3a8ed636f54f08635f35cc94d50637bd4ddb85d6
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2020-08-07 21:55:33 +0200

    cleaned up site_list.py and updated site.md

--- site_list.py
+++ site_list.py
@@ -1,19 +1,11 @@
 """Sherlock: Supported Site Listing
-This module generates the listing of supported sites.
+This module generates the listing of supported sites
+which can be found in sites.md
+It also organizes all the sites in alphanumeric order
 """
 import json
-import sys
-import requests
-import threading
-import xml.etree.ElementTree as ET
-from datetime import datetime
-from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 pool = list()
-
-parser = ArgumentParser(formatter_class=RawDescriptionHelpFormatter)
-
-args = parser.parse_args()
 
 with open("sherlock/resources/data.json", "r", encoding="utf-8") as data_file:
     data = json.load(data_file)
@@ -22,17 +14,19 @@ with open("sites.md", "w") as site_file:
     data_length = len(data)
     site_file.write(f'## List Of Supported Sites ({data_length} Sites In Total!)\n')
 
-    index = 1
-    for social_network, url_main, th in pool:
+    for social_network in data:
+        url_main = data.get(social_network).get("urlMain")
+        pool.append((social_network, url_main))
 
+    index = 1
+    for social_network, url_main in pool:
         site_file.write(f'{index}. [{social_network}]({url_main})\n')
-        sys.stdout.write("\r{0}".format(f"Updated {index} out of {data_length} entries"))
-        sys.stdout.flush()
         index = index + 1
+
 
 sorted_json_data = json.dumps(data, indent=2, sort_keys=True)
 
 with open("sherlock/resources/data.json", "w") as data_file:
     data_file.write(sorted_json_data)
 
-print("\nFinished updating supported site listing!")
+print("Finished updating supported site listing!")
