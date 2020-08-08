1fcae33003fb0c9789a1bbfb5b4948e0c0137a04
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2020-08-08 16:52:14 +0200

    version bump 0.12.3 --> 0.12.4

--- sherlock/sherlock.py
+++ sherlock/sherlock.py
@@ -25,7 +25,7 @@ from notify import QueryNotifyPrint
 from sites  import SitesInformation
 
 module_name = "Sherlock: Find Usernames Across Social Networks"
-__version__ = "0.12.3"
+__version__ = "0.12.4"
 
 
 
