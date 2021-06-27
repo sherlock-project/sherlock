d503b2d871a313703b784f6f1846bed8cf8a7439
Miltiadis Tsichlis <miltos04@gmail.com>
2021-06-27 20:31:28 +0300

    create dark mode java script file

---
+++ website/js/darkmode.js
@@ -0,0 +1,14 @@
+const options = {
+    bottom: '64px', 
+    right: '32px', 
+    left: 'unset', 
+    time: '0.3s', 
+    mixColor: '#fff', 
+    backgroundColor: '#fff', 
+    buttonColorDark: '#100f2c',  
+    buttonColorLight: '#fff', 
+    saveInCookies: true, 
+    label: '🌓', 
+    autoMatchOsTheme: true 
+  }
+new Darkmode(options).showWidget();
