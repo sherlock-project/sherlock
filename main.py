bf5e343117d4927039015bb2044f3498c5577a01
SHAURYA MAKWANA <82109937+SHAURYA-MAKWANA@users.noreply.github.com>
2022-03-19 13:46:26 +0300

    Create main.py
    so that a person can directly run main.py to use sherlock

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+
+username = input("enter your username to sherlock his online profiles: ")
+path = r"coding\fresh_python\sherlock\sherlock\sherlock.py"
+
+os.system(f"python {path} {username}")
