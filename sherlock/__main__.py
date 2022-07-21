56c36b734f0545f8f9f0414a5ab5295f8e9af0ba
Lev Ostatnigrosh <lev@levs-mbp-2.lan>
2022-07-20 20:05:01 -0400

    cleaned up __main__ to make it more uniform

--- sherlock/__main__.py
+++ sherlock/__main__.py
@@ -2,21 +2,23 @@
 
 """
 Sherlock: Find Usernames Across Social Networks Module
-
 This module contains the main logic to search for usernames at social
 networks.
 """
-
 import sys
+
+import sherlock
 
 
 if __name__ == "__main__":
     # Check if the user is using the correct version of Python
     python_version = sys.version.split()[0]
 
-    if sys.version_info < (3, 6):
-        print("Sherlock requires Python 3.6+\nYou are using Python %s, which is not supported by Sherlock" % (python_version))
+    if sys.version_info >= (3, 6):
+        sherlock.main()
+    else:
+        print(
+            "Sherlock requires Python 3.6+\nYou are using Python %s, which is not supported by Sherlock"
+            % (python_version)
+        )
         sys.exit(1)
-
-    import sherlock
-    sherlock.main()
