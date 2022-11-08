c1f9574be363e9b71989975f5835ec3a3b2b0961
DaniloSouzza <danilopaesdesouza@gmail.com>
2022-11-08 11:11:53 +0100

    fix: adding sherlock to path and importing from it

--- sherlock/__main__.py
+++ sherlock/__main__.py
@@ -8,15 +8,21 @@ networks.
 """
 
 import sys
+# Append sherlock to path
+# Considering that we're running the application from __main__
+# we could face importing problem (relative import)
+sys.path.insert(0, "../sherlock")
 
 
 if __name__ == "__main__":
     # Check if the user is using the correct version of Python
+    from sherlock import sherlock
+
+    
     python_version = sys.version.split()[0]
 
     if sys.version_info < (3, 6):
         print("Sherlock requires Python 3.6+\nYou are using Python %s, which is not supported by Sherlock" % (python_version))
         sys.exit(1)
 
-    import sherlock
     sherlock.main()
