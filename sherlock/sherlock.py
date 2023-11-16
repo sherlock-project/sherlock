07b82311cebf3d246856eab7139efa3019e93c95
AsrinTopal <oyuncukahraman15@gmail.com>
2023-11-16 21:32:29 +0200

    Update sherlock.py

--- sherlock/sherlock.py
+++ sherlock/sherlock.py
@@ -1,11 +1,3 @@
-#! /usr/bin/env python3
-
-"""
-Sherlock: Find Usernames Across Social Networks Module
-
-This module contains the main logic to search for usernames at social
-networks.
-"""
 
 import csv
 import signal
