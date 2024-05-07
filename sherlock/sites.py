25e0acd98e80e33e4c65086ce66cd3615d908d1c
Paul Pfeister <code@pfeister.dev>
2024-05-07 03:05:52 -0400

    Skip content filter for explicitly chosen targets
    Targets specified via --site will no longer be excluded when --nsfw is not set, even if flagged as nsfw. If the target was specifically listed by the user, it's probably because they want that target.
    
    Fixes sherlock-project/sherlock#2103

--- sherlock/sites.py
+++ sherlock/sites.py
@@ -175,7 +175,7 @@ class SitesInformation:
 
         return
 
-    def remove_nsfw_sites(self):
+    def remove_nsfw_sites(self, do_not_remove: list[str] = []):
         """
         Remove NSFW sites from the sites, if isNSFW flag is true for site
 
@@ -186,8 +186,9 @@ class SitesInformation:
         None
         """
         sites = {}
+        do_not_remove = [site.casefold() for site in do_not_remove]
         for site in self.sites:
-            if self.sites[site].is_nsfw:
+            if self.sites[site].is_nsfw and site.casefold() not in do_not_remove:
                 continue
             sites[site] = self.sites[site]  
         self.sites =  sites
