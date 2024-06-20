c5fa9d05ca84523463c607ab04bf64f1ed64fd8d
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2024-06-20 13:04:30 +0200

    Update site list to sherlockproject.xyz/sites
    With these changes, the site list will be written to the
    sherlock-project/sherlockproject.xyz repo where the docs and everything
    is located. This way, we wont have to manually update the list on the
    website.

--- devel/site-list.py
+++ devel/site-list.py
@@ -2,6 +2,7 @@
 # This module generates the listing of supported sites which can be found in
 # sites.md. It also organizes all the sites in alphanumeric order
 import json
+import os
 
 # Read the data.json file
 with open("sherlock/resources/data.json", "r", encoding="utf-8") as data_file:
@@ -14,13 +15,16 @@ social_networks.pop('$schema', None)
 # Sort the social networks in alphanumeric order
 social_networks: list = sorted(social_networks.items())
 
+# Make output dir where the site list will be written
+os.mkdir("output")
+
 # Write the list of supported sites to sites.md
-with open("../sites.md", "w") as site_file:
-    site_file.write(f"## List Of Supported Sites ({len(social_networks)} Sites In Total!)\n")
+with open("output/sites.mdx", "w") as site_file:
+    site_file.write(f"---\ntitle: 'List of supported sites'\nsidebarTitle: 'Supported sites'\nicon: 'globe'\ndescription: 'Sherlock currently supports **400+** sites'\n---\n\n")
     for social_network, info in social_networks:
         url_main = info["urlMain"]
         is_nsfw = "**(NSFW)**" if info.get("isNSFW") else ""
-        site_file.write(f"1. ![](https://www.google.com/s2/favicons?domain={url_main}) [{social_network}]({url_main}) {is_nsfw}\n")
+        site_file.write(f"1. [{social_network}]({url_main}) {is_nsfw}\n")
 
 # Overwrite the data.json file with sorted data
 with open("sherlock/resources/data.json", "w") as data_file:
@@ -29,3 +33,4 @@ with open("sherlock/resources/data.json", "w") as data_file:
     data_file.write("\n")
 
 print("Finished updating supported site listing!")
+
