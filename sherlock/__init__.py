0ece8bf672f0796b13d20df035b640c921ad823d
Paul Pfeister <code@pfeister.dev>
2024-06-30 22:32:31 -0400

    Except ImportErrors induced by legacy run method

--- sherlock/__init__.py
+++ sherlock/__init__.py
@@ -5,6 +5,9 @@ networks.
 
 """
 
+# This variable is only used to check for ImportErrors induced by users running as script rather than as module or package
+importErrTestVar = None
+
 __shortname__   = "Sherlock"
 __longname__    = "Sherlock: Find Usernames Across Social Networks"
 __version__     = "0.14.4"
