42ae66d28b36c4330eea8aff11058971276ba955
BimboBeggings <cimdinsatis@gmail.com>
2024-11-14 09:55:59 +0200

    Update test_ux.py

--- tests/test_ux.py
+++ tests/test_ux.py
@@ -31,7 +31,7 @@ def test_wildcard_username_expansion():
     assert sherlock.check_for_parameter('test{?test') is False
     assert sherlock.check_for_parameter('test?}test') is False
     assert sherlock.multiple_usernames('test{?}test') == ["test_test" , "test-test" , "test.test"]
-
+ 
 
 @pytest.mark.parametrize('cliargs', [
     '',
