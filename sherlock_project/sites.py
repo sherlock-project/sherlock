fc46670eace1fe033f4adb08ac6ae378a7fbcfd6
Kenneth Seet <kenurfseet@gmail.com>
2025-02-05 21:37:07 +0800

    style(format): reformat codebase using Black

--- sherlock_project/sites.py
+++ sherlock_project/sites.py
@@ -3,13 +3,23 @@
 This module supports storing information about websites.
 This is the raw data that will be used to search for usernames.
 """
+
 import json
 import requests
 import secrets
 
+
 class SiteInformation:
-    def __init__(self, name, url_home, url_username_format, username_claimed,
-                information, is_nsfw, username_unclaimed=secrets.token_urlsafe(10)):
+    def __init__(
+        self,
+        name,
+        url_home,
+        url_username_format,
+        username_claimed,
+        information,
+        is_nsfw,
+        username_unclaimed=secrets.token_urlsafe(10),
+    ):
         """Create Site Information Object.
 
         Contains information about a specific website.
@@ -54,7 +64,7 @@ class SiteInformation:
         self.username_claimed = username_claimed
         self.username_unclaimed = secrets.token_urlsafe(32)
         self.information = information
-        self.is_nsfw  = is_nsfw
+        self.is_nsfw = is_nsfw
 
         return
 
@@ -67,7 +77,7 @@ class SiteInformation:
         Return Value:
         Nicely formatted string to get information about this object.
         """
-        
+
         return f"{self.name} ({self.url_home})"
 
 
@@ -114,7 +124,9 @@ class SitesInformation:
 
         # Ensure that specified data file has correct extension.
         if not data_file_path.lower().endswith(".json"):
-            raise FileNotFoundError(f"Incorrect JSON file extension for data file '{data_file_path}'.")
+            raise FileNotFoundError(
+                f"Incorrect JSON file extension for data file '{data_file_path}'."
+            )
 
         # if "http://"  == data_file_path[:7].lower() or "https://" == data_file_path[:8].lower():
         if data_file_path.lower().startswith("http"):
@@ -127,9 +139,10 @@ class SitesInformation:
                 )
 
             if response.status_code != 200:
-                raise FileNotFoundError(f"Bad response while accessing "
-                                        f"data file URL '{data_file_path}'."
-                                        )
+                raise FileNotFoundError(
+                    f"Bad response while accessing "
+                    f"data file URL '{data_file_path}'."
+                )
             try:
                 site_data = response.json()
             except Exception as error:
@@ -149,11 +162,12 @@ class SitesInformation:
                         )
 
             except FileNotFoundError:
-                raise FileNotFoundError(f"Problem while attempting to access "
-                                        f"data file '{data_file_path}'."
-                                        )
-        
-        site_data.pop('$schema', None)
+                raise FileNotFoundError(
+                    f"Problem while attempting to access "
+                    f"data file '{data_file_path}'."
+                )
+
+        site_data.pop("$schema", None)
 
         self.sites = {}
 
@@ -161,21 +175,22 @@ class SitesInformation:
         for site_name in site_data:
             try:
 
-                self.sites[site_name] = \
-                    SiteInformation(site_name,
-                                    site_data[site_name]["urlMain"],
-                                    site_data[site_name]["url"],
-                                    site_data[site_name]["username_claimed"],
-                                    site_data[site_name],
-                                    site_data[site_name].get("isNSFW",False)
-
-                                    )
+                self.sites[site_name] = SiteInformation(
+                    site_name,
+                    site_data[site_name]["urlMain"],
+                    site_data[site_name]["url"],
+                    site_data[site_name]["username_claimed"],
+                    site_data[site_name],
+                    site_data[site_name].get("isNSFW", False),
+                )
             except KeyError as error:
                 raise ValueError(
                     f"Problem parsing json contents at '{data_file_path}':  Missing attribute {error}."
                 )
             except TypeError:
-                print(f"Encountered TypeError parsing json contents for target '{site_name}' at {data_file_path}\nSkipping target.\n")
+                print(
+                    f"Encountered TypeError parsing json contents for target '{site_name}' at {data_file_path}\nSkipping target.\n"
+                )
 
         return
 
@@ -194,8 +209,8 @@ class SitesInformation:
         for site in self.sites:
             if self.sites[site].is_nsfw and site.casefold() not in do_not_remove:
                 continue
-            sites[site] = self.sites[site]  
-        self.sites =  sites
+            sites[site] = self.sites[site]
+        self.sites = sites
 
     def site_name_list(self):
         """Get Site Name List.
