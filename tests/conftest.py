35940e7584a91cec015e4281f24bdeec7fe8cddc
Paul Pfeister <code@pfeister.dev>
2025-09-17 13:44:02 -0400

    fix: ignore exclusions list on parameterization for false positive tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,8 @@ import urllib
 import pytest
 from sherlock_project.sites import SitesInformation
 
-def fetch_local_manifest() -> dict[str, dict[str, str]]:
-    sites_obj = SitesInformation(data_file_path=os.path.join(os.path.dirname(__file__), "../sherlock_project/resources/data.json"))
+def fetch_local_manifest(honor_exclusions: bool = True) -> dict[str, dict[str, str]]:
+    sites_obj = SitesInformation(data_file_path=os.path.join(os.path.dirname(__file__), "../sherlock_project/resources/data.json"), honor_exclusions=honor_exclusions)
     sites_iterable = {site.name: site.information for site in sites_obj}
     return sites_iterable
 
@@ -27,7 +27,7 @@ def remote_schema():
 
 def pytest_generate_tests(metafunc):
     if "chunked_sites" in metafunc.fixturenames:
-        sites_info = fetch_local_manifest()
+        sites_info = fetch_local_manifest(honor_exclusions=False)
         params = [{name: data} for name, data in sites_info.items()]
         ids = list(sites_info.keys())
         metafunc.parametrize("chunked_sites", params, ids=ids)
