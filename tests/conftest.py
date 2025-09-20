435540606e950cad028182d209238b622f87b4a3
Paul Pfeister <code@pfeister.dev>
2025-09-20 13:49:29 -0400

    chore: add typedef

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from sherlock_project.sites import SitesInformation
 
 def fetch_local_manifest(honor_exclusions: bool = True) -> dict[str, dict[str, str]]:
     sites_obj = SitesInformation(data_file_path=os.path.join(os.path.dirname(__file__), "../sherlock_project/resources/data.json"), honor_exclusions=honor_exclusions)
-    sites_iterable = {site.name: site.information for site in sites_obj}
+    sites_iterable: dict[str, dict[str, str]] = {site.name: site.information for site in sites_obj}
     return sites_iterable
 
 @pytest.fixture()
