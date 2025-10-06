91ba5a46cd04e12b39953cfc973dca7f816ddb01
shreyasNaik0101 <shreyas.naik0805@gmail.com>
2025-10-06 23:32:26 +0530

    feat: Implement working site alias logic
    Co-authored-by: obiwan04kanobi <obiwan04kanobi@users.noreply.github.com>

--- sherlock_project/sherlock.py
+++ sherlock_project/sherlock.py
@@ -783,7 +783,11 @@ def main():
     # Eventually, the rest of the code will be updated to use the new object
     # directly, but this will glue the two pieces together.
     site_data_all = {site.name: site.information for site in sites}
-    if not args.site_list:
+    # Create original dictionary from SitesInformation() object.
+    # Eventually, the rest of the code will be updated to use the new object
+    # directly, but this will glue the two pieces together.
+    site_data_all = {site.name: site.information for site in sites}
+    if args.site_list == []:
         # Not desired to look at a sub-set of sites
         site_data = site_data_all
     else:
